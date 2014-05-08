b32bdb86a1b7a848a7f6b78d2c570b5f6e4816a9
Martin <ms349@hdm-stuttgart.de>
2014-05-08 18:19:08 +0200

    GUI Grundstruktur + HTML + CSS

---
+++ IT-Projekt 2014/src/de/hdm/itprojekt/client/gui/SocialMediaPinnwand.java
@@ -0,0 +1,22 @@
+package de.hdm.itprojekt.client.gui;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class SocialMediaPinnwand implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
