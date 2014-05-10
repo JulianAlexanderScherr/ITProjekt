3cff05fee93d690e06b20ece6d5caae38b63c014
Martin <ms349@hdm-stuttgart.de>
2014-05-10 12:33:20 +0200

    - Interface Verwaltungsklasse fertig gestellt - Interface VerwaltungsklasseAsynch fertig gestellt - KLasse VerwaltungsklasseImpl erweitert und alle Methoden eingefügt

--- IT-Projekt 2014/src/de/hdm/itprojekt/shared/Verwaltungsklasse.java
+++ IT-Projekt 2014/src/de/hdm/itprojekt/shared/Verwaltungsklasse.java
@@ -34,12 +34,12 @@ public interface Verwaltungsklasse extends RemoteService{
 	/**
 	 * Initialisierung des Objekts. Diese Methode ist vor dem Hintergrund von GWT
      * RPC zusätzlich zum No Argument Constructor der implementierenden Klasse
-     * {@link VerwaltungsklaseeImpl} notwendig. Bitte diese Methode direkt nach der
-	 * Instantiierung aufrufen.
+     * {@link VerwaltungsklaseeImpl} notwendig.
 	 * @throws IllegalArgumentException
 	 * @author Thies
 	 */
 	public void init() throws IllegalArgumentException;
+	
 	
 	/**
 	 * Eine Pinnwand anlegen, bzw mit einem Nutzer verknüpfen
@@ -57,6 +57,23 @@ public interface Verwaltungsklasse extends RemoteService{
 	 */
 	public void setKommentar(String text, Beitrag beitrag) throws IllegalArgumentException;
 	
+	
+	/**
+	 * Beitragstext erstellen
+	 * @param text
+	 * @throws IllegalArgumentException
+	 */
+	public void setBeitrag(String text) throws IllegalArgumentException;
+	
+	
+	/**
+	 * Setzen eines Abonnements
+	 * @param abonnement
+	 * @throws IllegalArgumentException
+	 */
+	public void setAbonnement(Abonnement abonnement) throws IllegalArgumentException;
+	
+	
 	/**
 	 * Kommentar loeschen
 	 * @param kommentar
@@ -66,18 +83,12 @@ public interface Verwaltungsklasse extends RemoteService{
 	
 	
 	/**
-	 * Beitragstext erstellen
-	 * @param text
-	 * @throws IllegalArgumentException
-	 */
-	public void setBeitrag(String text) throws IllegalArgumentException;
-	
-	/**
 	 * Einen Beitrag löschen
 	 * @param beitrag
 	 * @throws IllegalArgumentException
 	 */
 	public void loeschenBeitrag(Beitrag beitrag) throws IllegalArgumentException;
+	
 	
 	/**
 	 * Einen Beitrag liken (Kommentare können nicht geliked oder kommentiert werden)
@@ -87,13 +98,6 @@ public interface Verwaltungsklasse extends RemoteService{
 	 */
 	public void likeBeitrag(Beitrag beitrag, Nutzer nutzer) throws IllegalArgumentException;
 	
-	/**
-	 * Auslesen eines einzelnen Beitrag. Zurückgegeben wird das Beitrags Objekt
-	 * @param id
-	 * @return
-	 * @throws IllegalArgumentException
-	 */
-	public Beitrag getBeitrag(int id) throws IllegalArgumentException;
 	
 	/**
 	 * Einen geliketen Beitrag wieder entliken
@@ -103,6 +107,7 @@ public interface Verwaltungsklasse extends RemoteService{
 	 */
 	public void unlikeBeitrag(Beitrag beitrag, Nutzer nutzer) throws IllegalArgumentException;
 	
+	
 	/**
 	 * Auslesen aller Nutzer. Zurückgegeben wird ein Vector der alle Nutzer Objekte der Datenbank enthält
 	 * @return
@@ -110,4 +115,112 @@ public interface Verwaltungsklasse extends RemoteService{
 	 */
 	public Vector<Nutzer> getAlleNutzer() throws IllegalArgumentException;
 	
+	
+	/**
+	 * Auslesen aller Likes. Zurückgegeben wird ein Vector der alle Like Objekte der Datenbank enthält
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public Vector<Like> getAlleLikes() throws IllegalArgumentException; 
+	
+	
+	/**
+	 * Auslesen aller Beiträge. Zurückgegeben wird ein Vector der alle Beitrags Objekte der Datenbank enthält
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public Vector<Beitrag> getAlleBeitraege() throws IllegalArgumentException; 
+	
+	
+	/**
+	 * Auslesen aller Abonnenten. Zurückgegeben wird ein Vector der alle Abonnement Objekte der Datenbank enthält
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public Vector<Abonnement> getAlleAbonnenten() throws IllegalArgumentException; 
+	
+	
+	/**
+	 * Auslesen aller Businessobjekte. Zurückgegeben wird ein Vector der alle Inhalte der Datenbank enthält
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public Vector<BusinessObject> getAlleBusinessObjects() throws IllegalArgumentException;
+	
+	
+	/**
+	 * Auslesen eines einzelnen Beitrag. Zurückgegeben wird das Beitrags Objekt
+	 * @param id
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public Beitrag getBeitrag(int id) throws IllegalArgumentException;
+	
+	
+	/**
+	 * Ausgeben eines Nutzers anhand dessen ID
+	 * @param id
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public Nutzer getNutzerByID(int id) throws IllegalArgumentException;
+	
+	/**
+	 * Ausgeben von Nutzern durch Eingabe des Nachnamens
+	 * @param nachname
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public Vector<Nutzer> getNutzerByNachname(String nachname) throws IllegalArgumentException;
+	
+	
+	/**
+	 * Ausgeben von Nutzern durch Eingabe des Vornamens
+	 * @param vorname
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public Vector<Nutzer> getNutzerByVorname(String vorname) throws IllegalArgumentException;
+	
+	
+	/**
+	 * Ausgeben von Nutzern durch Eingabe des Nicknamen
+	 * @param nickname
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public Vector<Nutzer> getNutzerByNickname(String nickname) throws IllegalArgumentException;
+	
+	
+	/**
+	 * Ausgeben eines Pinnwand-Objektes anhand der ID
+	 * @param id
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public Pinnwand getPinnwand(int id) throws IllegalArgumentException;
+	
+	/**
+	 * Ausgeben eines Kommentar-Objektes anhand der ID
+	 * @param id
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public Kommentar getKommentar(int id) throws IllegalArgumentException;
+	
+	/**
+	 * Ausgeben eines Abonnement-Objektes anhand der ID
+	 * @param id
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public Abonnement getAbonnement(int id) throws IllegalArgumentException;
+	
+	/**
+	 * Ausgeben eines Like-Objektes anhand der ID
+	 * @param id
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public Like getLike(int id) throws IllegalArgumentException;
 }
